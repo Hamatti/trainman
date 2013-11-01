3a93e4fe7241affef0d6a156f7c9af83d05a0ad9
Josp <rotterr@netti.fi>
2013-11-01 19:08:37 +0200

    hero näkyviin ja level_new mukaan

--- src/scenes.js
+++ src/scenes.js
@@ -20,7 +20,7 @@ Crafty.scene( 'Game', function () {
     }
 
     // Player character, placed at 5, 5 on our grid
-    this.player = Crafty.e( 'PlayerCharacter' ).at( 5, 5 );
+    this.player = Crafty.e( 'PlayerCharacter' ).at( 5, 1 );
     this.occupied[this.player.at().x][this.player.at().y] = true;
 
     // Place a tree at every edge square on our grid of 16x16 tiles
