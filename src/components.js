5c72a40ab2620f3e3956151ec63b4cc696504a31
Juha-Matti Santala <juhamattisantala@gmail.com>
2013-11-01 20:25:20 +0200

    Fixed grammar error

--- src/components.js
+++ src/components.js
@@ -161,7 +161,7 @@ Crafty.c('Dialog', {
       "intro": "Welcome to TRAINMAN",
       "check_prompt": "Tickets, please",
       "check_success": "Your ticket is valid, have a nice trip!",
-      "check_failure": "WHAT?! You don't have a ticket? That's fine for you!",
+      "check_failure": "WHAT?! You don't have a ticket? That's a fine for you!",
       "tutorial": "You can move the Hero with arrows or WASD. <br />Interact with E"
   }
 
