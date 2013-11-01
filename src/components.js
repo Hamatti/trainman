f21e12e2894a4b3256bc40af44129a6a5de8eeae
Anssi Kinnunen <aatkin@utu.fi>
2013-11-01 14:11:51 +0200

    pashaa ja pashaa

--- src/components.js
+++ src/components.js
@@ -24,63 +24,84 @@ Crafty.c('Grid', {
 Crafty.c('Actor', {
   init: function() {
     this.requires('2D, Canvas, Grid');
-  },
+  }
+});
+
+// A solid wall
+Crafty.c('Wall_borderless', {
+    init: function() {
+        this.requires('Actor, Solid, spr_wall_noborders');
+    }
+});
+
+// A solid wall
+Crafty.c('Wall_window', {
+    init: function() {
+        this.requires('Actor, Solid, spr_wall_window');
+    }
+});
+
+// A solid wall
+Crafty.c('Wall_jallu', {
+    init: function() {
+        this.requires('Actor, Solid, spr_wall_jallu');
+    }
 });
 
 // A Tree is just an Actor with a certain sprite
 Crafty.c('Tree', {
   init: function() {
     this.requires('Actor, Solid, spr_tree');
-  },
+  }
 });
 
 // A Bush is just an Actor with a certain sprite
 Crafty.c('Bush', {
   init: function() {
     this.requires('Actor, Solid, spr_bush');
-  },
+  }
 });
 
 // A Rock is just an Actor with a certain sprite
 Crafty.c('Rock', {
   init: function() {
     this.requires('Actor, Solid, spr_rock');
-  },
+  }
 });
 
 // This is the player-controlled character
 Crafty.c('PlayerCharacter', {
   init: function() {
-    this.requires('Actor, Fourway, Collision, spr_player, SpriteAnimation')
+    this.requires('Actor, Fourway, Collision, spr_trainplayer, SpriteAnimation')
       .fourway(2)
       .stopOnSolids()
-      .onHit('Village', this.visitVillage)
+      .onHit('Village', this.visitVillage);
       // These next lines define our four animations
       //  each call to .animate specifies:
       //  - the name of the animation
       //  - the x and y coordinates within the sprite
       //     map at which the animation set begins
       //  - the number of animation frames *in addition to* the first one
-      .animate('PlayerMovingUp',    0, 0, 2)
-      .animate('PlayerMovingRight', 0, 1, 2)
-      .animate('PlayerMovingDown',  0, 2, 2)
-      .animate('PlayerMovingLeft',  0, 3, 2);
+//      .animate('PlayerMovingUp',    1, 1, 0)
+//      .animate('PlayerMovingRight', 1, 1, 0)
+//      .animate('PlayerMovingDown',  1, 1, 0)
+//      .animate('PlayerMovingLeft',  1, 1, 0);
 
     // Watch for a change of direction and switch animations accordingly
-    var animation_speed = 4;
-    this.bind('NewDirection', function(data) {
-      if (data.x > 0) {
-        this.animate('PlayerMovingRight', animation_speed, -1);
-      } else if (data.x < 0) {
-        this.animate('PlayerMovingLeft', animation_speed, -1);
-      } else if (data.y > 0) {
-        this.animate('PlayerMovingDown', animation_speed, -1);
-      } else if (data.y < 0) {
-        this.animate('PlayerMovingUp', animation_speed, -1);
-      } else {
-        this.stop();
-      }
-    });
+//    var animation_speed = 4;
+//    this.bind('NewDirection', function(data) {
+//      if (data.x > 0) {
+//        this.animate('PlayerMovingRight', animation_speed, -1);
+//      } else if (data.x < 0) {
+//        this.animate('PlayerMovingLeft', animation_speed, -1);
+//      } else if (data.y > 0) {
+//        this.animate('PlayerMovingDown', animation_speed, -1);
+//      } else if (data.y < 0) {
+//        this.animate('PlayerMovingUp', animation_speed, -1);
+//      } else {
+//        this.stop();
+//      }
+//    });
   },
 
   // Registers a stop-movement function to be called when
