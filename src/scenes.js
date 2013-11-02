3cfef274c4b7a74d3cf1bfa6bdf90cce7af6c72c
Juha-Matti Santala <jumasan@utu.fi>
2013-11-02 13:33:28 +0200

    Only interact with customers in adjacent grid cells

--- src/scenes.js
+++ src/scenes.js
@@ -19,16 +19,23 @@ Crafty.scene( 'Game', function () {
         }
     }
 
-    // Place a tree at every edge square on our grid of 16x16 tiles
+    // A 2D array to keep track of all passeneger positions
+    this.passengers = new Array(Game.map_grid.width);
+    for ( var i = 0; i < Game.map_grid.width; i++ ) {
+        this.passengers[i] = new Array( Game.map_grid.height );
+        for ( var y = 0; y < Game.map_grid.height; y++ ) {
+            this.passengers[i][y] = false;
+        }
+    }    
+
+    // Insert car elements
     for ( var x = 0; x < Game.map_grid.width; x++ ) {
         for ( var y = 0; y < Game.map_grid.height; y++ ) {
             Crafty.e( 'Floor_light' ).at(x,y);
             var at_edge = x == 0 || x == Game.map_grid.width - 1 ||  y == 0;
             var tile;
             if ( at_edge ) {
-                // Place a tree entity at the current tile
-				// Crafty.e( 'Tree' ).at( x, y );
-				if (y == 0) {
+                if (y == 0) {
 					if (x == 0 || x == Game.map_grid.width - 1) {tile = 'Wall_vr';} 
 					else if (x < 5) {tile = 'Wall_boozes';}
 					else if (x == 7 || x == 9 || x == 10 || x == 12) {tile = 'Wall_window';}
@@ -56,62 +63,35 @@ Crafty.scene( 'Game', function () {
             //---- MANUALLY INSERTED PASSENGERS ----//
             if(x==1 && y==5) {
                 Crafty.e( 'Woman1_right' ).at(x,y);
+                this.passengers[x][y] = true;
             }
             if(x==7 && y==2) {
                 Crafty.e( 'Teen_right' ).at(x,y);
+                this.passengers[x][y] = true;
             }
             if(x==6 && y==5) {
                 Crafty.e( 'Child2_left' ).at(x,y);
+                this.passengers[x][y] = true;
             }
             this.occupied[x][y] = true;
-            
-//			 else if ( Math.random() < 0.06 && !this.occupied[x][y] ) {
-//                // Place a bush entity at the current tile
-//                var bush_or_rock = (Math.random() > 0.3) ? 'Bush' : 'Rock';
-//                Crafty.e( bush_or_rock ).at( x, y );
-//                this.occupied[x][y] = true;
-//            }
+
         }
     }
     // Player character, placed at 5, 5 on our grid
     this.player = Crafty.e( 'PlayerCharacter' ).at( 5, 1 );
     this.occupied[this.player.at().x][this.player.at().y] = true;
 
-//    // Generate five villages on the map in random locations
-//    var max_villages = 10;
-//    for ( var x = 0; x < Game.map_grid.width; x++ ) {
-//        for ( var y = 0; y < Game.map_grid.height; y++ ) {
-//            if ( Math.random() < 0.03 ) {
-//                if ( Crafty( 'Village' ).length < max_villages && !this.occupied[x][y] ) {
-//                    Crafty.e( 'Village' ).at( x, y );
-//                }
-//            }
-//        }
-//    }
-
     // -------------------- START THE GAME -------------------/
     // Play a ringing sound to indicate the start of the journey
     Crafty.audio.play( 'ring' );
 
-    // Show the victory screen once all villages are visisted
-    this.show_victory = this.bind( 'VillageVisited', function () {
-        if ( !Crafty( 'Village' ).length ) {
-            Crafty.scene( 'Victory' );
-        }
-    } );
-
     this.interactable = this.bind('Interactable', function(data) {
-        if (this.occupied[data.x+1][data.y] || this.occupied[data.x][data.y+1] || this.occupied[data.x-1][data.y] || this.occupied[data.x][data.y-1]) {
+        if (this.passengers[data.x+1][data.y] || this.passengers[data.x][data.y+1] || this.passengers[data.x-1][data.y] || this.passengers[data.x][data.y-1]) {
             interact(data.player);
         }
     });
 
-}, function () {
-    // Remove our event binding from above so that we don't
-    //  end up having multiple redundant event watchers after
-    //  multiple restarts of the game
-    this.unbind( 'VillageVisited', this.show_victory );
-} );
+});
 
 
 // Victory scene
