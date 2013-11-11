ea3b91229185fa02322f59ff4981d6d29d565ffc
Hamatti <jumasan@utu.fi>
2013-11-11 13:21:34 +0100

    Commented the backend file

--- src/php-backend/new_passenger.php
+++ src/php-backend/new_passenger.php
@@ -1,23 +1,32 @@
 <?
+
+    // All data for passenger generation is in this folder
     $DATADIR = '../../assets/passengerdata/';
+
+    // Sex from POST request: if no valid, use male
     $wanted_sex = ($_POST['sex'] == 'M' || $_POST['sex'] == 'F') ? $_POST['sex'] : 'M';
+
+    // Read data in to arrays
     $male_names = file($DATADIR . 'miesten_etunimet.txt');
     $female_names = file($DATADIR . 'naisten_etunimet.txt');
     $surnames = file($DATADIR . 'sukunimet.txt');
     $hometowns = file($DATADIR . 'towns.txt');
     $occupations = file($DATADIR . 'ammatit.txt');
 
+    // Define which first name list to use
     $first_names;
     if($wanted_sex == 'M') $first_names = $male_names;
     else $first_names = $female_names;
 
-    $first_name =$first_names[array_rand($first_names)];
-    $first_name = trim($first_name);
-    $surname = $surnames[array_rand($surnames)];
-    $surname = trim($surname);
+
+    // Randomly select data attributs
+    $first_name = trim($first_names[array_rand($first_names)]);
+    $surname = trim($surnames[array_rand($surnames)]);
     $name = $first_name . ' ' . $surname;
-    $occupation = $occupations[array_rand($occupations)];
-    $occupation = trim($occupation);
+    $occupation = trim($occupations[array_rand($occupations)]);
     $hometown = trim($hometowns[array_rand($hometowns)]);
-    echo json_encode(array("name"=> $name,"home"=> $hometown, "occupation"=>$occupation));
+    $age = rand(10,70);
+
+    // Return as JSON string
+    echo json_encode(array("name"=> $name,"home"=> $hometown, "occupation"=>$occupation, "age"=>$age));
 ?>
