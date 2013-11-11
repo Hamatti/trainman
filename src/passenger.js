e6a141c73da761078cd16ee9c0d9b0651fd319f7
Hamatti <jumasan@utu.fi>
2013-11-11 13:21:56 +0100

    Added sprite-name generating based on age and sex

--- src/passenger.js
+++ src/passenger.js
@@ -1,24 +1,35 @@
 /* Data structure for passenger */
 
-function Passenger(name, home, occupation, in_train) {
-    this.x = null;
-    this.y = null;
+function Passenger(name, home, occupation, age, sex, in_train) {
+    this.x;
+    this.y;
     this.does_have_ticket = function() { return (Math.random() < 0.95) ? true : false;};
     this.board = function(x, y) {
         this.is_in_train = true;
         this.x = x;
         this.y = y;
     }
+    this.age = age;
     this.name = name;
     this.home = home;
+    this.sex = sex;
     this.occupation = occupation;
     this.hasTicket = this.does_have_ticket();
     this.is_in_train = in_train;
+    this.direction;
+    this.set_direction = function(dir) { this.direction = dir; };
+    this.age_group;
+    if(this.age >= 10 && this.age <= 15) this.age_group = "child";
+    else if(this.age >15 && this.age <= 25) this.age_group = "teenager";
+    else if (this.age > 25 && this.age <= 50) this.age_group = "adult";
+    else this.age_group = "old";
+
+    this.sprite = function() { return this.age_group + "_" + this.sex.toLowerCase() + "_" + this.direction; };
 }
 
 
 function create_passenger(sex) {
     passenger_info = $.parseJSON(passenger_creator.new_passenger(sex));
-    var passenger = new Passenger(passenger_info.name, passenger_info.home, passenger_info.occupation, true);
+    var passenger = new Passenger(passenger_info.name, passenger_info.home, passenger_info.occupation, passenger_info.age, sex, true);
     return passenger;
 }
