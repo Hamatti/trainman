e82ec99f269485af34251d4cf72f912756b0535e
Hamatti <jumasan@utu.fi>
2013-11-11 14:18:12 +0100

    Removed comment

--- src/scenes.js
+++ src/scenes.js
@@ -38,7 +38,6 @@ Crafty.scene( 'Game', function () {
         }
     } 
 
-	//-------End of car creation------//
     // Insert car elements
     for ( var x = 0; x < Game.map_grid.width; x++ ) {
         for ( var y = 0; y < Game.map_grid.height; y++ ) {
